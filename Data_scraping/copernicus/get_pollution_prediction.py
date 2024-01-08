063cd05c11db5597cc3215ce23e18ab790e0d039
Veronika Deketová <veronika.deketova.uni@gmail.com>
2024-01-08 22:55:25 +0100

    update copernicus prediction

--- Data_scraping/copernicus/get_pollution_prediction.py
+++ Data_scraping/copernicus/get_pollution_prediction.py
@@ -4,42 +4,59 @@ import os
 import subprocess
 import random
 
-pollutant_id_conversion = {'NO2': 'no2', 'PM10': 'pm10', 'PM25': 'pm2p5', # our mapping vs ECMWF mapping
-                           'SO2': 'so2', 'O3': 'go3'}
+pollutant_id_conversion = {'PM10': 'pm10', 'PM25': 'pm2p5', # our mapping vs ECMWF mapping
+                           'SO2': 'so2', 'O3': 'go3', 'NO2': 'no2'}
+pollutant_measure_type = {'NO2': 'ml', 'PM10': 'sfc', 'PM25': 'sfc', # our mapping vs ECMWF mapping
+                           'SO2': 'ml', 'O3': 'ml'}
 
 # file format naming axample: z_cams_c_ecmf_20240107120000_prod_fc_ml_024_go3.grid (24 steps prediction for go3 to reference date 20240107120000)
+# z_cams_c_ecmf_20240108000000_prod_fc_sfc_024_pm2p5.grid
 
-def get_pollution_data(stations_data: dict, pollutant: str, date: str ="20240107120000", prediction_steps: int = 24) -> pd.DataFrame:
-    file_format = f"z_cams_c_ecmf_{date}_prod_fc_ml_0{prediction_steps}_{pollutant}.grid"
+def get_pollution_data(stations_data: dict, pollutant: str, type: str, date: str ="20240108000000", prediction_steps: int = 36) -> pd.DataFrame:
+    file_format = f"z_cams_c_ecmf_{date}_prod_fc_{type}_0{prediction_steps}_{pollutant}.grib"
     latitue = stations_data['LATITUDE']
-    longitude = stations_data['LONGTITUDE']
-    command = f"grib_ls -l {latitue},{longitude},1 -j -f -p dataDate,dataTime,validityDate,validityTime {file_format}" # -j stands for json output
-    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
-    print(result.stdout)
+    longitude = stations_data['LONGITUDE']
+    command = f"grib_ls -l {latitue},{longitude},1 -j -p dataDate,dataTime,validityDate,validityTime prediction_data/{file_format}" # -j stands for json output
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+    #print(result.stdout)
     data = json.loads(result.stdout)
-    return data
+    print("Pollutant processed", pollutant)
+    print(len(data))
+    #print(data)
+    list_matches = data[-1]['neighbours'] #first model level
+    min_item_index = 0
+    min_item_distance = 2000
+    for index, item in enumerate(list_matches):
+        if item['distance'] < min_item_distance:
+            min_item_index = index
+            min_item_distance = item['distance']
+    print("value before", list_matches[min_item_index]['value'])
+    min_distance_pred_value = float(list_matches[min_item_index]['value'])*(10**9) # convert to ug/m3 from kg/m3
+    print("min distance value", min_distance_pred_value, list_matches[min_item_index]['index'])
+    print("unit", list_matches[min_item_index]['unit'])
+    print("distance", list_matches[min_item_index]['distance'])
+    return min_distance_pred_value
 
 def read_pollution_stations(path_to_json: str) -> dict:
     with open(path_to_json, 'r') as f:
         data = json.load(f)
     return data
 
-if __name__ =="__main__":
-    stations_data = read_pollution_stations("daily_pollution_stations_converted.json")
+def get_prediction_data():
+    stations_data = read_pollution_stations("daily_pollution_stations_converted_filtered.json")
+    print("len station data", len(stations_data))
     datetime = "20240107120000"
-    prediction_steps = 24 # how many hours in advance we want to predict
+    prediction_steps = 36 # how many hours in advance we want to predict
     for pullutant in pollutant_id_conversion:
         prediction_data = {"SAMPLINGPOINT_LOCALID":[], "DATETIME":[], "PREDICATION_STEP": [], "PROPERTY": [], "VALUE_NUMERIC": [], "UNIT": [], "STATIONCODE": [], "STATIONNAME": [], "LONGITUDE": [], "LATITUDE": [], "MUNICIPALITY": []}
         for station in stations_data:
             print(station)
-            #station_data = get_pollution_data(stations_data[station], pollutant_id_conversion[pullutant], datetime, prediction_steps)
+            predicted_value = get_pollution_data(stations_data=stations_data[station], pollutant=pollutant_id_conversion[pullutant],type=pollutant_measure_type[pullutant])
             prediction_data["SAMPLINGPOINT_LOCALID"].append(station)
             prediction_data["DATETIME"].append(datetime)
             prediction_data["PREDICATION_STEP"].append(prediction_steps)
             prediction_data["PROPERTY"].append(pullutant)
-            #prediction_data["VALUE_NUMERIC"].append(station_data["data"][0]["value"])
-            # rand float value between 1 and 10
-            prediction_data["VALUE_NUMERIC"].append(random.uniform(1, 10))
+            prediction_data["VALUE_NUMERIC"].append(predicted_value)
             prediction_data["UNIT"].append('ug/m3')
             prediction_data["STATIONCODE"].append(station)
             prediction_data["STATIONNAME"].append(stations_data[station]["STATION_NAME"])
@@ -50,5 +67,7 @@ if __name__ =="__main__":
         # check whether folder exists
         if not os.path.exists("prediction_data"):
             os.makedirs("prediction_data")
-        df.to_csv(f"prediction_data/prediction_data_{pullutant}_{datetime}_{prediction_steps}.csv", index=False)
+        df.to_csv(f"prediction_data/cop_prediction_data_{pullutant}_{datetime}_{prediction_steps}.csv", index=False)
         
+if __name__ =="__main__":
+    get_prediction_data()
--- Data_scraping/eea/eea_pollution_history.py
+++ Data_scraping/eea/eea_pollution_history.py
@@ -2,74 +2,129 @@ import os
 import pandas as pd
 import json
 
-pollutant_id_conversion = {'NO2': '8', 'PM10': '5', 'PM25': '6001',
-                           'SO2': '1', 'O3': '7'}
+POLLUTANT_ID_CONVERSION = {'8':'NO2', '5': 'PM10', '6001': 'PM25',
+                            '1': 'SO2', '7': 'O3'}
 
-#get unique station codes
-station_codes = set()
+LEFT_UP = {"LAT": 46.47713492720725, "LONG":10.72030291968136}
+LEFT_BOTTOM = {"LAT":45.922571254640175,"LONG": 10.56311627043269}
+RIGHT_UP = {"LAT": 46.4704290320345, "LONG":11.824782562358994}
+RIGHT_BOTTOM = {"LAT":45.83735326997908, "LONG":11.216901449777854}
+
+STATION_CODES = set()
 for file in os.listdir("csv_conversion/"):
     if file.endswith(".csv"):
-        print(file)
+        #print(file)
         code = file.split("_")[0]
-        station_codes.add(code)
-
-print(station_codes)
-print(len(station_codes))
-
-name_mappings_LAT = {}
-name_mappings_LONG = {}
-name_mappings_MUN = {}
-name_mappings_STAT_CODE = {}
-name_mappings_STATIONNAME = {}
-name_station_mappings_location = {}
-
-not_found = set()
+        STATION_CODES.add(code)
 
 
-# loop through the files in the current folder
-for file in os.listdir("converted_daily/"):
-    if file.endswith(".csv"):
-        daily_file = pd.read_csv("converted_daily/" + file)
-        column_values_long_names = daily_file['SAMPLINGPOINT_LOCALID'].values
-        for code in station_codes:
-            print(code)
-            for value in column_values_long_names:
-                    if code in value:
-                        print("found")
-                        row_value = daily_file.loc[daily_file['SAMPLINGPOINT_LOCALID']== value]
-                        LATITUDE = row_value['LATITUDE'].values[0]
-                        LONGITUDE = row_value['LONGITUDE'].values[0]
-                        MUNICIPALITY = row_value['MUNICIPALITY'].values[0]
-                        STATION_CODE = row_value['STATIONCODE'].values[0]
-                        STATION_NAME = row_value['STATIONNAME'].values[0]
-                        if code not in name_mappings_LAT:
-                            name_station_mappings_location[code] =  {}
-                        """ name_mappings_LAT[code] = []
-                            name_mappings_LONG[code] = []
-                            name_mappings_MUN[code] = []
-                            name_mappings_STAT_CODE[code] = []
-                            name_mappings_STATIONNAME[code] = []
-                        name_mappings_LAT[code].append(LATITUDE)
-                        name_mappings_LONG[code].append(LONGITUDE)
-                        name_mappings_MUN[code].append(MUNICIPALITY)
-                        name_mappings_STAT_CODE[code].append(STATION_CODE)
-                        name_mappings_STATIONNAME[code].append(STATION_NAME) """
-                        name_station_mappings_location[code]['STATION_CODE'] = STATION_CODE
-                        name_station_mappings_location[code]['STATION_NAME'] = STATION_NAME
-                        name_station_mappings_location[code]['LATITUDE'] = LATITUDE
-                        name_station_mappings_location[code]['LONGTITUDE'] = LONGITUDE
-                        name_station_mappings_location[code]['MUNICIPALITY'] = MUNICIPALITY
-                    else:
-                        not_found.add(code)
 
-#print(name_mappings_MUN)
-#print(name_mappings_STAT_CODE)
-#print(name_mappings_STATIONNAME)
+def get_history_stations():
 
-#save station name and station municipality
-print(name_station_mappings_location)
-with open('history_data_stations_location.json', 'w') as file:
-    json.dump(name_station_mappings_location, file)
 
-print("not found:", len(not_found))
-print("found length:", len(name_mappings_LAT))
+    #get unique station codes
+
+
+    print(STATION_CODES)
+    print(len(STATION_CODES))
+
+    name_mappings_LAT = {}
+    name_mappings_LONG = {}
+    name_mappings_MUN = {}
+    name_mappings_STAT_CODE = {}
+    name_mappings_STATIONNAME = {}
+    name_station_mappings_location = {}
+
+    not_found = set()
+
+
+    # loop through the files in the current folder
+    for file in os.listdir("converted_daily/"):
+        if file.endswith(".csv"):
+            daily_file = pd.read_csv("converted_daily/" + file)
+            column_values_long_names = daily_file['SAMPLINGPOINT_LOCALID'].values
+            for code in STATION_CODES:
+                #print(code)
+                for value in column_values_long_names:
+                        if code in value:
+                            #print("found")
+                            row_value = daily_file.loc[daily_file['SAMPLINGPOINT_LOCALID']== value]
+                            LATITUDE = row_value['LATITUDE'].values[0]
+                            LONGITUDE = row_value['LONGITUDE'].values[0]
+                            if LATITUDE < LEFT_BOTTOM['LAT'] or LATITUDE > LEFT_UP['LAT']:
+                                continue
+                            if LONGITUDE < LEFT_UP['LONG'] or LONGITUDE > RIGHT_UP['LONG']:
+                                continue
+                            MUNICIPALITY = row_value['MUNICIPALITY'].values[0]
+                            STATION_CODE = row_value['STATIONCODE'].values[0]
+                            STATION_NAME = row_value['STATIONNAME'].values[0]
+                            if code not in name_mappings_LAT:
+                                name_station_mappings_location[code] =  {}
+                            """ name_mappings_LAT[code] = []
+                                name_mappings_LONG[code] = []
+                                name_mappings_MUN[code] = []
+                                name_mappings_STAT_CODE[code] = []
+                                name_mappings_STATIONNAME[code] = []
+                            name_mappings_LAT[code].append(LATITUDE)
+                            name_mappings_LONG[code].append(LONGITUDE)
+                            name_mappings_MUN[code].append(MUNICIPALITY)
+                            name_mappings_STAT_CODE[code].append(STATION_CODE)
+                            name_mappings_STATIONNAME[code].append(STATION_NAME) """
+                            name_station_mappings_location[code]['STATION_CODE'] = STATION_CODE
+                            name_station_mappings_location[code]['STATION_NAME'] = STATION_NAME
+                            name_station_mappings_location[code]['LATITUDE'] = LATITUDE
+                            name_station_mappings_location[code]['LONGTITUDE'] = LONGITUDE
+                            name_station_mappings_location[code]['MUNICIPALITY'] = MUNICIPALITY
+                        else:
+                            not_found.add(code)
+
+    #save station name and station municipality
+    print(len(name_station_mappings_location))
+    with open('history_data_stations_location_filtered.json', 'w') as file:
+        json.dump(name_station_mappings_location, file)
+
+    #print("not found:", len(not_found))
+    #print("found length:", len(name_mappings_LAT))
+
+def merge_filter_data():
+    station_codes_found_pollutants = []
+    NO2_data = pd.DataFrame()
+    PM10_data = pd.DataFrame()
+    PM25_data = pd.DataFrame()
+    SO2_data = pd.DataFrame()
+    O3_data = pd.DataFrame()
+    # ['38', '5', '5015', '8', '7', '8', '6001', '7', '9', '5', '9']
+    # ['38', '5', '5015', '8', '7', '8', '6001', '7', '9', '5', '9']
+    dataframe_pollutant_code = {"8": NO2_data, "5": PM10_data, "6001": PM25_data, "1": SO2_data, "7": O3_data}
+    filtered_stations_data = {}
+    with open('history_data_stations_location_filtered.json', 'r') as file: # 'daily_pollution_stations_converted_filtered.json'
+        filtered_stations_data = json.load(file)
+    pollutant_codes = list(POLLUTANT_ID_CONVERSION.keys())
+    for file in os.listdir("csv_conversion/"):
+        if file.endswith(".csv"):
+            pollutant_code = file.split("_")[1].rstrip(".csv")
+            station_code = file.split("_")[0]
+            if station_code not in list(filtered_stations_data.keys()):
+                continue
+            print(pollutant_code)
+            print(pollutant_codes)
+            station_codes_found_pollutants.append(pollutant_code)
+            if pollutant_code not in pollutant_codes:
+                continue
+            print("Found match")
+            pollutant_data = pd.read_csv("csv_conversion/" + file)
+            dataframe_pollutant_code[pollutant_code] = pd.concat([dataframe_pollutant_code[pollutant_code], pollutant_data], ignore_index=True)
+    # create folder if doesnt exist
+    if not os.path.exists("historical_data"):
+        os.makedirs("historical_data")
+    for pollutant_code in dataframe_pollutant_code:
+        dataframe_pollutant_code[pollutant_code].to_csv(f"historical_data/historical_{POLLUTANT_ID_CONVERSION[pollutant_code]}.csv", index=False)
+    print(station_codes_found_pollutants)
+    
+
+
+
+
+if __name__ == "__main__":
+    #get_history_stations()
+    merge_filter_data()
